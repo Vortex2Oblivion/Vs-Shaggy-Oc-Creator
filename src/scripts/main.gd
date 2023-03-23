b5566052007bf49002714492a09f98be8f8d8cfd
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-03-23 16:26:25 -0400

    change head type and save oc as a png

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
-@onready var color = $"Torso".modulate
 
 func _process(_delta):
 	$"Torso".modulate = Color8($"TorsoR".value as int,$"TorsoG".value as int,$"TorsoB".value as int)
 	$"Legs".modulate = Color8($"PantsR".value as int,$"PantsG".value as int,$"PantsB".value as int)
+	$Head.play(str($"HeadSlider".value))
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_button_pressed():
+	$"/root".size=Vector2(350,750)
+	$ScreenshotTimer.start()
+
+func _on_screenshot_timer_timeout():
+	var image = get_viewport().get_texture().get_image()
+	image.save_png("res://shaggy_oc.png")
+	$ResetWindowSize.start()
+
+func _on_reset_window_size_timeout():
+	$"/root".size=Vector2(650,750)
+
