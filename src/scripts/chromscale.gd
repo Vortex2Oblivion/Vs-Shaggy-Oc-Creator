96d99bb0e2582b1eef6028ddb17ff207a3b32474
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-04-17 16:59:44 -0400

    first bits of progress on chromatic scale generator

--- src/scripts/chromscale.gd
+++ src/scripts/chromscale.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+var playback
+
+func _process(delta):
+	$"ChromaticScale".pitch_scale = $"Pitch".value
+
 func _ready():
 	$"/root".size=Vector2(650,350)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+	
 func _on_play_button_pressed():
+
+	playback = $ChromaticScale.stream
+
 	$ChromaticScale.play()
+
+
+func _on_save_pressed():
+	$ChromaticScale.play()
+	playback = $ChromaticScale.stream
+	playback.save_to_wav("res://chromatic.wav")
